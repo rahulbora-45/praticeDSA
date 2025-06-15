3ce3f8f8b4672db85f0a74e885ecfe968442e80d
rahulbora-45 <152955978+rahulbora-45@users.noreply.github.com>
2025-06-15 08:26:44 +0530

    Add files via upload

---
+++ supreme3.0 assignment/debugExcercise.cpp
@@ -0,0 +1,145 @@
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+// 	char c;
+//     cin>>c;
+//     if('a'<=c || c<= 'z'){
+//         cout<<0;
+//     }
+// 	else if('A'<=c || c<= 'Z'){
+//         cout<<1;
+//     }
+//     else{
+//         cout<<-1;
+//     }
+// }
+
+
+
+
+
+/*
+Pattern
+1
+23
+345
+4567
+*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int i=1;
+//     while(i<=n){
+//         int count =1;
+//         int j=i;
+//         while(count<=i){
+//             cout<<j;
+//             j=j+1;
+
+//             count++;
+//         }
+//         cout<<"\n";
+//         i = i + 1;
+//     }
+// }
+
+
+/*
+Pattern
+N = 4
+   *
+  ***
+ *****
+*******
+*/
+
+
+
+
+/*
+Pattern
+N = 4
+1
+22
+333
+4444
+*/
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int i,j,n;
+//     cin>>n;
+//     for(i=1;i<=n;i++){
+//         for(j=1;j<=i;j++){
+// 	        cout<<i;
+//         }
+//     cout<<endl;
+//     }
+// }
+
+
+
+/*
+Pattern
+N = 4
+1
+21
+321
+4321
+*/
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//   int i,j,n;
+//     cin>>n;
+//     for(i=1;i<=n;i++){
+//        int p=i;
+//         for(j=1;j<=i;j++){
+//           cout<<p;
+//           p--;
+//         }
+//         cout<<endl;
+        
+//     }
+// }
+
+
+
+// /*
+// Pattern
+// N = 5
+// E
+// DE
+// CDE
+// BCDE
+// ABCDE
+// */
+
+// #include<iostream>
+// using namespace std;
+
+// int main() {
+//     int i,j,n;;
+//     cin>>n;
+//     for(i=1;i<=n;i++){
+//         char z=i;
+//         char p='A'+n-i;
+//         for(j=1;j<=i;j++){
+//             cout<<p;
+//             p++;  
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
